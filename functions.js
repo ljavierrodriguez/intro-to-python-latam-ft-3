5af7e34d30bcb6b326e253dc11be45ca9fb4c1b0
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2025-05-12 15:01:30 -0400

    Intro to Python

--- functions.js
+++ functions.js
@@ -1,1 +1,39 @@
 // Funciones
+/* 
+
+1. Funcion de Nombre 
+
+function nombreFuncion(params...){
+    codigo
+}
+
+2. Funciones Anonimas
+
+const variable = function(params...){
+    codigo
+}
+
+3. Funciones Flecha
+
+const nombreFuncion = (params...) => retorno
+
+*/
+
+function saludar(){
+    console.log("Hola Mundo!")
+}
+
+saludar()
+
+const sumar = (a, b) => a + b
+
+
+sumar(10, 15)
+
+
+function imprimirNombre(nombre="John Doe"){
+    return `Hola, ${nombre}`
+}
+
+imprimirNombre("Luis") // Hola Luis
+imprimirNombre() // Hola John Doe
--- functions.py
+++ functions.py
@@ -1,1 +1,58 @@
 # Funciones
+
+""" 
+1. Funcion de Nombre
+
+def nombre_funcion(params...):
+    codigo
+
+2. Funciones Lambda
+
+nombre_funcion = lambda params : retorno
+
+"""
+
+def saludar():
+    print("Hola Mundo!")
+
+saludar() # Hola Mundo!
+
+sumar = lambda a, b : a + b
+
+print(sumar(10, 10)) # 20
+
+
+# 1. Parametros Posicionales
+
+sumar(10, 20)
+
+# 2. Parametros por Defecto
+
+def imprimir_nombre(nombre="John Doe"):
+    return f"Hola, {nombre}"
+
+print(imprimir_nombre("Luis")) # Hola, Luis
+print(imprimir_nombre()) # Hola, John Doe
+
+# 3. Parametros Keywords
+
+def informacion_personal(nombre = "", apellido = "", edad = ""):
+    return f"Nombre: {nombre} {apellido} Edad: {edad}"
+
+print(informacion_personal(edad=43, apellido="Doe", nombre="Jane"))
+
+# 4. Empaquetamiento de Parametros
+
+def totalizar(*args):
+    resultado = sum(args)
+    return resultado
+
+print(totalizar(1, 2, 3, 4, 5)) # 15
+
+# 5. Empaquetamiento de Parametros Keywords
+
+def informacion_personal_key(**kargs):
+    return f"Nombre: {kargs["nombre"]} {kargs["apellido"]} Edad: {kargs["edad"]}"
+
+print(informacion_personal_key(edad=43, apellido="Doe", nombre="Jane"))
+# Nombre: Jane Doe Edad: 43
